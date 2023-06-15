3c165b0c48b6163de7f8fba7e495f8bc13e81f58
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-06-15 21:34:04 +0300

    upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import re
+from typing import List
+
 import requests
-from fastapi import FastAPI, Request, status
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
+
 from Mongo import MongoParser
 import sys
 
@@ -26,20 +30,74 @@ async def parse_product(spu):
     mongo_parser.set_database('dewu_shop')
     mongo_parser.set_collection('products')
 
-    spu = parse_spu(spu)
-    if not spu:
+    new_spu = parse_spu(spu)
+    if not new_spu:
         return {"error": "spuId is empty"}
 
-    parsed = list(mongo_parser.collection.find({"detail.spuId": int(spu)}))
+    # if spu != new_spu:
+    #     return RedirectResponse(f'/get_product/?spu={new_spu}')
+
+    parsed = list(mongo_parser.collection.find({"detail.spuId": int(new_spu)}))
     if not parsed:
-        result = mongo_parser.add_item(spu)
+        result = mongo_parser.add_item(new_spu)
         if not result:
             return {"error": "incorrect spuId"}
-        return await parse_product(spu)
     parsed[0].pop('_id')
     return parsed[0]
 
 
-@app.get("/get_product/")
+class Brand(BaseModel):
+    brandId: int
+    brandName: str
+
+
+class Detail(BaseModel):
+    spuId: int
+    categoryId: int
+    brandId: int
+    authPrice: int
+    logoUrl: str
+    title: str
+    subTitle: str
+    desc: str
+    sourceName: str
+    articleNumber: str
+    articleNumbers: list
+    sellDate: str
+    fitId: int
+    brandLogoUrl: str
+    brandList: List[Brand]
+    STORY: str
+    INTRODUCTION: str
+    SHOW: str
+    DETAIL: str
+    properties: dict
+
+
+class Size(BaseModel):
+    sizeKey: str
+    sizeValue: str
+
+
+class Variant(BaseModel):
+    propertyId: int
+    name: str
+    value: str
+    propertyValueId: int
+    level: int
+    customValue: str
+    sort: int
+    definitionId: int
+    skuId: int
+
+
+class Product(BaseModel):
+    detail: Detail
+    images: List[str]
+    sizeInfo: List[Size]
+    variants: List[Variant]
+
+
+@app.get("/get_product/", response_model=Product)
 async def get_product(spu: str):
     return await parse_product(spu)
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+from pprint import pprint
+
 import requests
 
-r = requests.get('http://194.58.109.219:8000/web/get_product/1871373')
-print(r.json())
-print(r.headers)
+# spu = 'https://dw4.co/t/A/1Gzc1gsi'
+spu = '2444035'
+r = requests.get('http://194.58.109.219:8000/api/v3/redoc',
+                 params={
+                     'apikey': 'K9nhzyw1YxrUa2ODuHJoCLbOr8bxHWFR',
+                     'spu': spu
+                 })
+pprint(r.text)
